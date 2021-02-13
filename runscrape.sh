26bd8924f9310ca152b5296954e9ba2a2c2bb3eb
Simone Farre <simon.farre.cx@gmail.com>
2021-02-13 16:41:38 +0100

    added and changed bash scripts for executing scraping

--- runscrape.sh
+++ runscrape.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-rm test -rf
-mkdir test
 
 cargo.exe build --release 
 cp target/release/hockeyscraper.exe ./hockeyscraper
 
-./hockeyscraper -d test -y 2020 -g 868 -e 20210204 --jobs 12
+./hockeyscraper -d test -y 2020 -g 868 --jobs 12
--- setup_rustffi_assets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# The FFI library exists in the same workspace folder (one level up, then in project folder is stattrends, therefore ../stattrends)
-
-
-cargo.exe build --release 
-cp target/release/hockeyscraper.exe ./hockeyscraper
-
-./hockeyscraper -d ../stattrends -y 2020 -g 868 --jobs 12
-
